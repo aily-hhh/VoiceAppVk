34abe1c61024f6777a131c319925273f8a82e532
Aily-Hhh <78144867+aily-hhh@users.noreply.github.com>
2023-03-02 00:56:00 +0300

    update
    Vk SDK

--- app/src/main/java/com/hhh/voiceappvk/MainActivity.kt
+++ app/src/main/java/com/hhh/voiceappvk/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.hhh.voiceappvk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        VK.login(this, arrayListOf(VKScope.DOCS))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object: VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                Toast.makeText(this@MainActivity, "DONE", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                Toast.makeText(this@MainActivity, "ERROR", Toast.LENGTH_SHORT).show()
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
